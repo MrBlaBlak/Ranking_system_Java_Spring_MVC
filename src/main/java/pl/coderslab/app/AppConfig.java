7b5ef4cda8720efb9832fee96cc34562010aca9c
walaniam <mariusz.walania@gmail.com>
2023-11-28 13:54:33 +0100

    Run with docker

--- src/main/java/pl/coderslab/app/AppConfig.java
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -1,5 +1,6 @@
 package pl.coderslab.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ import javax.persistence.EntityManagerFactory;
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.coderslab")
 @EnableTransactionManagement
+@Slf4j
 public class AppConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver viewResolver() {
@@ -41,6 +43,7 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     }
     @Bean
     public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        log.info("Initializing JpaTransactionManager...");
         return new JpaTransactionManager(entityManagerFactory);
     }
 }
