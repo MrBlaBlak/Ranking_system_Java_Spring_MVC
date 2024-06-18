fa12b0e8613dcb86cd3c5541afe83427643b08bd
Michał Romak <mrblablak@gmail.com>
2024-06-18 22:52:41 +0200

    Test: fixed testExceptionHandling()

--- src/test/java/mrblablak/ranking/workshop/service/LobbyServiceTest.java
+++ src/test/java/mrblablak/ranking/workshop/service/LobbyServiceTest.java
@@ -1,41 +1,22 @@
 package mrblablak.ranking.workshop.service;
 
-import mrblablak.ranking.workshop.dtoForForms.GamersDTO;
-import mrblablak.ranking.workshop.dtoForForms.GamersMatchStatsDTO;
 import mrblablak.ranking.workshop.model.*;
 import mrblablak.ranking.workshop.repository.*;
+import mrblablak.ranking.workshop.service.lobby.impl.LobbyService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class LobbyServiceTest {
     @Mock
     private GamerRepository gamerRepository;
-
-    @Mock
-    private MatchRepository matchRepository;
-
-    @Mock
-    private TeamRepository teamRepository;
-
-    @Mock
-    private KillsAndCapsRepository killsAndCapsRepository;
-
-    @Mock
-    private MatchGamerRepository matchGamerRepository;
 
     @InjectMocks
     private LobbyService lobbyService;
@@ -51,47 +32,5 @@ class LobbyServiceTest {
         List<Gamer> actualGamers = lobbyService.getAllGamers();
         assertEquals(expectedGamers, actualGamers);
         verify(gamerRepository, times(1)).findAll();
-
     }
-
-    @Test
-    void setHandicap() {
-    }
-
-    @Test
-    void checkPerfectBalance() {
-
-    }
-    @Test
-    void checkTeam() {
-
-    }
-    @Test
-    void calculateMmr() {
-    }
-    @Test
-    void setMatch() {
-    }
-
-    @Test
-    void setTeams() {
-    }
-
-    @Test
-    void updateGamers() {
-    }
-
-    @Test
-    void setMatchGamers() {
-    }
-
-    @Test
-    void setKillsAndCaps() {
-    }
-
-    @Test
-    void saveData() {
-    }
-
-
 }
--- src/test/java/mrblablak/ranking/workshop/service/lobby/impl/DataHandlerImplTest.java
+++ src/test/java/mrblablak/ranking/workshop/service/lobby/impl/DataHandlerImplTest.java
@@ -14,14 +14,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-
 import java.util.Optional;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 public class DataHandlerImplTest {
-
+    private static final int TEAM_SIZE = 5;
+    private static final int LOBBY_SIZE = 10;
     @Mock
     private GamerRepository gamerRepository;
 
@@ -40,17 +39,13 @@ public class DataHandlerImplTest {
     @Mock
     private MmrCalculator mmrCalculator;
 
-    @Mock
-    private Logger logger;
-
     @InjectMocks
     private DataHandlerImpl dataHandler;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp()  {
 
     }
-
     @Test
     public void testValidDataHandling() {
         // Prepare valid input
@@ -59,7 +54,7 @@ public class DataHandlerImplTest {
         Gamer[] team2gamers = createValidTeam2GamersArray();
 
         // Mock GamerRepository behavior
-        for (int i = 0; i < team1gamers.length; i++) {
+        for (int i = 0; i < TEAM_SIZE; i++) {
             when(gamerRepository.findById(gamersMatchStatsDTO.getTeam1gamersId()[i]))
                     .thenReturn(Optional.of(team1gamers[i]));
             when(gamerRepository.findById(gamersMatchStatsDTO.getTeam2gamersId()[i]))
@@ -89,19 +84,19 @@ public class DataHandlerImplTest {
         assertTrue(result);
         // Add further assertions to verify data saving and state
         verify(matchRepository, times(1)).save(any());
-        verify(teamRepository, times(2)).save(any()); // team1 and team2
-        verify(gamerRepository, times(2 * team1gamers.length)).save(any()); // team1gamers and team2gamers
-        verify(matchGamerRepository, times(10)).save(any()); // 10 matchGamers
-        verify(killsAndCapsRepository, times(10)).save(any()); // 10 killsAndCaps
+        verify(teamRepository, times(2)).save(any());
+        verify(gamerRepository, times(LOBBY_SIZE)).save(any());
+        verify(matchGamerRepository, times(LOBBY_SIZE)).save(any());
+        verify(killsAndCapsRepository, times(LOBBY_SIZE)).save(any());
     }
     @Test
     public void testExceptionHandling() {
 
         GamersMatchStatsDTO gamersMatchStatsDTO = createValidGamersMatchStatsDTO();
-        Gamer[] team1gamers = createValidTeam1GamersArray();
-        Gamer[] team2gamers = createValidTeam2GamersArray();
+        Gamer[] team1gamers = createinValidTeam1GamersArray();
+        Gamer[] team2gamers = createinValidTeam2GamersArray();
 
-        for (int i = 0; i < team1gamers.length; i++) {
+        for (int i = 0; i < TEAM_SIZE; i++) {
             when(gamerRepository.findById(gamersMatchStatsDTO.getTeam1gamersId()[i]))
                     .thenReturn(Optional.of(team1gamers[i]));
             when(gamerRepository.findById(gamersMatchStatsDTO.getTeam2gamersId()[i]))
@@ -109,12 +104,9 @@ public class DataHandlerImplTest {
         }
         when(mmrCalculator.calculateMmr(anyInt(), any(), any(), any(), any(), anyBoolean()))
                 .thenReturn(true);
-        doThrow(new RuntimeException("Database error")).when(matchRepository).save(any());
 
         // Execute and expect an exception to be thrown
         assertThrows(Exception.class, () -> dataHandler.setData(gamersMatchStatsDTO, team1gamers, team2gamers));
-
-        // Verify that logger.error() was called with the exception
 
     }
 
@@ -138,42 +130,61 @@ public class DataHandlerImplTest {
 
 
     private Gamer[] createValidTeam1GamersArray() {
-        // Implement method to create a valid Gamer[] for team1
-        Gamer player1 = new Gamer("Player1", 1500.0, "Server1", "WWLLWWLLWW");
+
+        Gamer player1 = new Gamer("Player1", 1500.0, "Server1", "1010101010");
         player1.setId(1);
 
-        Gamer player2 = new Gamer("Player2", 1600.0, "Server1", "LWLWLWLWLW");
+        Gamer player2 = new Gamer("Player2", 1600.0, "Server1", "1010101010");
         player2.setId(2);
 
-        Gamer player3 = new Gamer("Player3", 1700.0, "Server1", "WLWLWLWLWL");
+        Gamer player3 = new Gamer("Player3", 1700.0, "Server1", "1010101010");
         player3.setId(3);
 
-        Gamer player4 = new Gamer("Player4", 1800.0, "Server1", "WWWWLLLLWW");
+        Gamer player4 = new Gamer("Player4", 1800.0, "Server1", "1010101010");
         player4.setId(4);
 
-        Gamer player5 = new Gamer("Player5", 1900.0, "Server1", "LLLWWWWLLL");
+        Gamer player5 = new Gamer("Player5", 1900.0, "Server1", "1010101010");
         player5.setId(5);
 
         return new Gamer[] { player1, player2, player3, player4, player5 };
     }
 
     private Gamer[] createValidTeam2GamersArray() {
-        // Implement method to create a valid Gamer[] for team2
-        Gamer player6 = new Gamer("Player6", 1500.0, "Server1", "LLWWLLWWLL");
+
+        Gamer player6 = new Gamer("Player6", 1500.0, "Server1", "1010101010");
         player6.setId(6);
 
-        Gamer player7 = new Gamer("Player7", 1600.0, "Server1", "WLWLWLWLWL");
+        Gamer player7 = new Gamer("Player7", 1600.0, "Server1", "1010101010");
         player7.setId(7);
 
-        Gamer player8 = new Gamer("Player8", 1700.0, "Server1", "LWLWLWLWLW");
+        Gamer player8 = new Gamer("Player8", 1700.0, "Server1", "1010101010");
         player8.setId(8);
 
-        Gamer player9 = new Gamer("Player9", 1800.0, "Server1", "LLLLWWWWLL");
+        Gamer player9 = new Gamer("Player9", 1800.0, "Server1", "1010101010");
         player9.setId(9);
 
-        Gamer player10 = new Gamer("Player10", 1900.0, "Server1", "WWWWLLLLWW");
+        Gamer player10 = new Gamer("Player10", 1900.0, "Server1", "1010101010");
         player10.setId(10);
 
+        return new Gamer[] { player6, player7, player8, player9, player10 };
+    }
+    private Gamer[] createinValidTeam1GamersArray() {
+
+        Gamer player1 = new Gamer("Player1", 1500.0, "Server1", "1010101010");
+        Gamer player2 = new Gamer("Player2", 1600.0, "Server1", "1010101010");
+        Gamer player3 = new Gamer("Player3", 1700.0, "Server1", "1010101010");
+        Gamer player4 = new Gamer("Player4", 1800.0, "Server1", "1010101010");
+        Gamer player5 = new Gamer("Player5", 1900.0, "Server1", "1010101010");
+        return new Gamer[] { player1, player2, player3, player4, player5 };
+    }
+
+    private Gamer[] createinValidTeam2GamersArray() {
+
+        Gamer player6 = new Gamer("Player6", 1500.0, "Server1", "1010101010");
+        Gamer player7 = new Gamer("Player7", 1600.0, "Server1", "1010101010");
+        Gamer player8 = new Gamer("Player8", 1700.0, "Server1", "1010101010");
+        Gamer player9 = new Gamer("Player9", 1800.0, "Server1", "1010101010");
+        Gamer player10 = new Gamer("Player10", 1900.0, "Server1", "1010101010");
         return new Gamer[] { player6, player7, player8, player9, player10 };
     }
 }
